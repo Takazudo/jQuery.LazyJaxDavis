f3a083203c50a83d25621b280e3f4653deb4ef2f
Takazudo <takazudo@gmail.com>
2012-04-06 06:05:22 +0900

    bumped version number to 0.1.1

--- jquery.lazyjaxdavis.js
+++ jquery.lazyjaxdavis.js
@@ -1,4 +1,4 @@
-/*! jQuery.LazyJaxDavis - v0.1.0 -  4/6/2012
+/*! jQuery.LazyJaxDavis - v0.1.1 -  4/6/2012
  * https://github.com/Takazudo/jQuery.LazyJaxDavix
  * Copyright (c) 2012 "Takazudo" Takeshi Takatsudo; Licensed MIT */
 
